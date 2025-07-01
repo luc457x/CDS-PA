d3aad8b318c1b2dc5ccdb0553d26d591be6ed401
luc457x <lucas7eixeir4@outlook.com>
2025-06-30 23:46:30 -0300

    clear: not used funcs. fix: plot_correlation() function

--- utils.py
+++ utils.py
@@ -1,23 +1,19 @@
 import pandas as pd
 import numpy as np
 import plotly.express as px
-import re
 import folium
+from haversine import haversine
 
 pd.set_option("display.max_columns", None)
 pd.set_option('future.no_silent_downcasting', True)
 
-def checkNaN(df):
-    """Recebe um DataFrame e retorna a quantidade de valores nulos e não nulos."""
-    null_count = df.isna().sum()
-    non_null_count = df.notnull().sum()
-    print(f"Null values:\n {null_count}")
-    print(f"Non-null values:\n {non_null_count}")
-    return
-
-def checkOutliers(df):
-    """Recebe um DataFrame e retorna um DataFrame com os outliers.
-    Baseia-se no pressuposto de que os dados estão normalmente distribuídos."""
+def check_outliers(df):
+    """
+    Recebe um DataFrame e retorna um DataFrame com os outliers.
+    Baseia-se no pressuposto de que os dados estão normalmente distribuídos.
+    
+    :param df: DataFrame a ser verificado
+    """
     outliers_list = []
     # Itera sobre as colunas numéricas
     for column in df.select_dtypes(include=[np.number]).columns:
@@ -34,55 +30,33 @@ def checkOutliers(df):
         outliers = df[(df[column] < lower_bound) | (df[column] > upper_bound)]
         outliers_list.append(outliers)
     # Concatena todos os outliers identificados
-    dfOutliers = pd.concat(outliers_list).drop_duplicates().reset_index(drop=True)
-    return dfOutliers
+    df_outliers = pd.concat(outliers_list).drop_duplicates().reset_index(drop=True)
+    return df_outliers
 
-def corr(df):
-    plt.figure(figsize=(20,16))
-    sns.heatmap(df.select_dtypes(include=[np.number]).corr(), annot=True)
-    plt.show()
-    return
-
-def hist(df):
-    # Recebe um DataFrame e plota um histograma para cada coluna numérica.
-    df.hist(bins=50, figsize=(25,16))
-    plt.show()
-    return
-
-def extract_words(column_name):
-    # Encontra todas as palavras dentro das aspas simples
-    matches = re.findall(r"\'(.*?)\'", column_name)
-    # Remove espaços extras e junta as palavras encontradas
-    return ' '.join(match.strip() for match in matches)
-
-def check_columns_for_set(column, value_set):
-    """Encontra as colunas que contem apenas subconjuntos de um conjunto de valores."""
-    non_nan_values = column.dropna()
-    return non_nan_values.isin(value_set).all()
-
-
-def extract_last_number(text):
-    # Verifica se o input é diferente de string ou Series do pandas
-    if not isinstance(text, (str, pd.Series)):
-        # Retorna o própio input
-        return text
-    # Verifica se o input é uma string
-    if isinstance(text, str):
-        # Regex para encontrar todos os números na string
-        numbers = re.findall(r"\b\d[\d.,]*\b", text)
-        # Se houver pelo menos dois números, retorna o último
-        if len(numbers) > 1:
-            return numbers[-1]
-        # Se houver apenas um número, retorna esse número
-        elif len(numbers) == 1:
-            return numbers[0]
-        # Se não houver números, retorna 0
-        else:
-            return 0
-    # Verifica se o input é uma Series do pandas
-    elif isinstance(text, pd.Series):
-        # Aplica a função a cada elemento da Series, a transformando em string
-        return text.apply(extract_last_number)
+def plot_correlation(df):
+    """
+    Plota uma heatmap de correlação para um DataFrame.
     
-def calc_percent(val, total):
-    return val.div((total), fill_value=0).replace([np.inf, -np.inf], 0)
+    :param df: DataFrame a ser analisado
+    """
+    correlation_matrix = df.select_dtypes(include=[np.number]).corr()
+    fig = px.imshow(correlation_matrix,
+                    title="Correlation Matrix",
+                    labels={"x": "Features", "y": "Features"},
+                    range_color=(-1, 1),
+                    color_continuous_scale="RdBu")
+    for i in range(len(correlation_matrix)):
+        for j in range(len(correlation_matrix.columns)):
+            fig.add_annotation(
+                text=str(round(correlation_matrix.iat[i, j], 2)),
+                x=i,
+                y=j,
+                showarrow=False,
+                font=dict(color='black')
+            )
+    fig.update_layout(coloraxis_showscale=True, coloraxis_cmin=-1, coloraxis_cmax=1,
+                    title={"x": 0.5, "xanchor": "center", "font": {"size": 24}},
+                    xaxis={"title_font": {"size": 18}, "showgrid": True},
+                    yaxis={"title_font": {"size": 18}, "showgrid": True})
+    return fig
+
