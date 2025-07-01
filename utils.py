0a069c75c277f2536efc695196453f092637ca7e
luc457x <lucas7eixeir4@outlook.com>
2025-07-01 01:00:56 -0300

    readd: plot_histogram function

--- utils.py
+++ utils.py
@@ -1,43 +1,67 @@
+import folium
 import pandas as pd
 import numpy as np
 import plotly.express as px
-import folium
+import plotly.subplots as ps
 from haversine import haversine
 
 pd.set_option("display.max_columns", None)
 pd.set_option('future.no_silent_downcasting', True)
 
-def check_outliers(df):
+def check_outliers(df, bounds=1.5):
     """
-    Recebe um DataFrame e retorna um DataFrame com os outliers.
-    Baseia-se no pressuposto de que os dados estão normalmente distribuídos.
+    Receives a DataFrame and return another Dataframe with only outliers.
+    Take as base the assumption that the data is normally distributed.
     
-    :param df: DataFrame a ser verificado
+    :param df: DataFrame to be analyzed.
+    :param bounds: Multiplier for IQR to determine outliers, smaller number means greater sensitivity.
+    :return: DataFrame with outliers.
     """
     outliers_list = []
-    # Itera sobre as colunas numéricas
+    # Loop through each numeric column in the DataFrame
     for column in df.select_dtypes(include=[np.number]).columns:
-        # Calcula o 1 quartil (Q1)
-        Q1 = df[column].quantile(0.25)
-        # Calcula o 3 quartil (Q3)
-        Q3 = df[column].quantile(0.75)
-        # Calcula o Intervalo Interquartil (IQR)
-        IQR = Q3 - Q1
-        # Define os limites inferior e superior para outliers
-        lower_bound = Q1 - 1.5 * IQR
-        upper_bound = Q3 + 1.5 * IQR
-        # Identifica os outliers da coluna atual e adiciona à lista
+        # Calculate the 1 quartil (Q1)
+        q1 = df[column].quantile(0.25)
+        # Calculate the 3 quartil (Q3)
+        q3 = df[column].quantile(0.75)
+        # Calculate the Interquartile Range (IQR)
+        iqr = q3 - q1
+        # Define the upper and lower bounds for outliers
+        lower_bound = q1 - bounds * iqr
+        upper_bound = q3 + bounds * iqr
+        # Identify the outliers and add them to the list
         outliers = df[(df[column] < lower_bound) | (df[column] > upper_bound)]
         outliers_list.append(outliers)
-    # Concatena todos os outliers identificados
+    # Concatenate all outliers identified
     df_outliers = pd.concat(outliers_list).drop_duplicates().reset_index(drop=True)
     return df_outliers
 
+def plot_histogram(df):
+    """
+    Plot a histogram for each numerical value in the given DataFrame.
+    
+    :param df: DataFrame to be analyzed.
+    :return:Plotly figure object with histograms for each numerical column in the DataFrame.
+    """
+    numeric_columns = df.select_dtypes(include=[np.number]).columns
+    nrows = int(np.ceil(np.sqrt(len(numeric_columns))))
+    ncols = int(np.ceil(len(numeric_columns) / nrows))
+    fig = ps.make_subplots(rows=nrows, cols=ncols, title="Numeric Columns", subplot_titles=numeric_columns)
+    for i, column in enumerate(numeric_columns):
+        row_idx = (i // ncols) + 1
+        col_idx = (i % ncols) + 1
+        fig.add_trace(px.histogram(df, x=column).data[0], row=row_idx, col=col_idx)
+    fig.update_layout(title={"x": 0.5, "xanchor": "center", "font": {"size": 24}},
+    xaxis={"title_font": {"size": 18}, "showgrid": True},
+    yaxis={"title_font": {"size": 18}, "showgrid": True})
+    return fig
+
 def plot_correlation(df):
     """
-    Plota uma heatmap de correlação para um DataFrame.
+    Plot a correlation matrix for the given DataFrame.
     
-    :param df: DataFrame a ser analisado
+    :param df: DataFrame to be analysed.
+    :return: Plotly figure object with the correlation matrix plot.
     """
     correlation_matrix = df.select_dtypes(include=[np.number]).corr()
     fig = px.imshow(correlation_matrix,
