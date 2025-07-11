d5db29e8b651d92e8de7cbe86aec0e3de6cf6deb
luc457x <lucas7eixeir4@outlook.com>
2025-07-10 22:00:34 -0300

    minor changes

--- libs/__init__.py
+++ libs/__init__.py
@@ -1,5 +1,5 @@
 import builtins
-from .utils import pd, np, clear_data, load_dataset, check_outliers, plot_histogram, plot_correlation
+from .utils import pd, np, px, ps, folium, haversine, clear_data, load_dataset, check_outliers, plot_histogram, plot_correlation
 __all__ = ["pd", "np"]
 
 for lib in __all__:
