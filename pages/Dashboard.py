96907fd31b71ff6272f41dcf727cf6c49d5737fd
luc457x <lucas7eixeir4@outlook.com>
2025-07-15 20:30:37 -0300

    finished: Dashboard streamlit page

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -30,10 +30,10 @@ with col1:
     if st.button("Company view"):
         st.session_state.visualizacao = 1
 with col2:
-    if st.button("Restaurant view"):
+    if st.button("Delivery view"):
         st.session_state.visualizacao = 2
 with col3:
-    if st.button("Delivery view"):
+    if st.button("Restaurant view"):
         st.session_state.visualizacao = 3
 st.sidebar.markdown("""---""")
 st.sidebar.write("**Data range**")
@@ -49,40 +49,51 @@ age_min, age_max = st.sidebar.slider(
     value=(df_clear["Delivery_person_Age"].min(), df_clear["Delivery_person_Age"].max()),
     step=1
 )
+rate_min, rate_max = st.sidebar.slider(
+    "Delivery person rating:",
+    min_value=df_clear["Delivery_person_Ratings"].min(),
+    max_value=df_clear["Delivery_person_Ratings"].max(),
+    value=(df_clear["Delivery_person_Ratings"].min(), df_clear["Delivery_person_Ratings"].max()),
+    step=0.1
+)
 st.sidebar.markdown("""---""")
 st.sidebar.write("**Filters**")
 traffic_options = st.sidebar.multiselect("Traffic conditions:", ["Low", "Medium", "High", "Jam", "Unknown"], default=[])
 city_options = st.sidebar.multiselect("City types:", ["Metropolitan", "Urban", "Semi-Urban", "Unknown"], default=[])
+order_options = st.sidebar.multiselect("Order type:", ["Buffet", "Drinks", "Meal", "Snack"], default=[])
 festival_options = st.sidebar.multiselect("During festivals:", ["Yes", "No", "Unknown"], default=[])
 # Filters
 df_clear = df_clear[(pd.to_datetime(df_clear["Time_Ordered"]).dt.date >= date_init) & (pd.to_datetime(df_clear["Time_Ordered"]).dt.date <= date_end)]
 df_clear = df_clear[(df_clear["Delivery_person_Age"] >= age_min) & (df_clear["Delivery_person_Age"] <= age_max)]
+df_clear = df_clear[(df_clear["Delivery_person_Ratings"] >= rate_min) & (df_clear["Delivery_person_Ratings"] <= rate_max)]
 if traffic_options: df_clear = df_clear[df_clear["Road_traffic_density"].isin(traffic_options)]
 if city_options: df_clear = df_clear[df_clear["City"].isin(city_options)]
+if order_options: df_clear = df_clear[df_clear["Type_of_order"].isin(order_options)]
 if festival_options: df_clear = df_clear[df_clear["Festival"].isin(festival_options)]
 ## Main page
 # body
 def viz1():
-    tab1, tab2, tab3 = st.tabs(["Tático", "Gerencial", "Geográfico"], )
+    st.title("Company View")
+    tab1, tab2, tab3 = st.tabs(["Tactical", "Managerial", "Geographical"], )
     with tab1:
         with st.container(border=True):
             col1, col2, col3, col4, col5 = st.columns([1, 1, 1, 1, 1], gap="small")
             comp_metrics = get_metrics_company(df_clear)
             with col1:
-                st.metric("Pedidos no período", int(comp_metrics["total_deliveries"]), width="content")
-                st.metric("Média semanal", int(comp_metrics["week_mean_deliveries"]), width="content")
+                st.metric("Total orders:", int(comp_metrics["total_deliveries"]), width="content")
+                st.metric("Weekly mean:", int(comp_metrics["week_mean_deliveries"]), width="content")
             with col2:
-                st.metric("Restaurantes ativos", comp_metrics["restaurants"])
-                st.metric("Desvio padrão semanal", int(comp_metrics["week_std_dev_deliveries"]))
+                st.metric("Restaurants:", comp_metrics["restaurants"])
+                st.metric("Weekly variability:", int(comp_metrics["week_std_dev_deliveries"]))
             with col3:
-                st.metric("Serviços de entrega ativos", comp_metrics["delivery_services"])
-                st.metric("Variação média semanal", int(comp_metrics["week_mean_diff_deliveries"]), f"{(comp_metrics["week_mean_diff_deliveries"] / comp_metrics["total_deliveries"]) * 100:.2f}%")
+                st.metric("Delivery services:", comp_metrics["delivery_services"])
+                st.metric("Mean weekly growth/decline:", int(comp_metrics["week_mean_diff_deliveries"]), f"{(comp_metrics["week_mean_diff_deliveries"] / comp_metrics["total_deliveries"]) * 100:.2f}%")
             with col4:
-                st.metric("Média de pedidos por restaurante", int(comp_metrics["mean_orders_per_restaurant"]))
-                st.metric("Melhor semana", int(comp_metrics["week_max_deliveries"]), f"{((comp_metrics["week_max_deliveries"] - comp_metrics["week_mean_deliveries"]) / comp_metrics["week_mean_deliveries"]) * 100:.2f}%")
+                st.metric("Mean orders per restaurant:", int(comp_metrics["mean_orders_per_restaurant"]))
+                st.metric("Best week:", int(comp_metrics["week_max_deliveries"]), f"{((comp_metrics["week_max_deliveries"] - comp_metrics["week_mean_deliveries"]) / comp_metrics["week_mean_deliveries"]) * 100:.2f}%")
             with col5:
-                st.metric("Média de pedidos por serviço de entrega", int(comp_metrics["mean_deliveries_per_service"]))
-                st.metric("Pior semana", int(comp_metrics["week_min_deliveries"]), f"{-100 if np.isinf(((comp_metrics["week_min_deliveries"] - comp_metrics["week_mean_deliveries"]) / comp_metrics["week_mean_deliveries"]) * 100) else ((comp_metrics["week_min_deliveries"] - comp_metrics["week_mean_deliveries"]) / comp_metrics["week_mean_deliveries"]) * 100:.2f}%")
+                st.metric("Mean orders per delivery service:", int(comp_metrics["mean_deliveries_per_service"]))
+                st.metric("Worst week:", int(comp_metrics["week_min_deliveries"]), f"{-100 if np.isinf(((comp_metrics["week_min_deliveries"] - comp_metrics["week_mean_deliveries"]) / comp_metrics["week_mean_deliveries"]) * 100) else ((comp_metrics["week_min_deliveries"] - comp_metrics["week_mean_deliveries"]) / comp_metrics["week_mean_deliveries"]) * 100:.2f}%")
         with st.container():
             col1, col2 = st.columns([1, 1], gap="medium")
             with col1:
@@ -101,19 +112,87 @@ def viz1():
         with st.container():
             col1, col2, col3 = st.columns([1, 1, 1])
             with col1:
-                st.write("Localização central de entregas por tipo de cidade e tráfego")
+                st.write("Delivery central locations grouped by city type and traffic:")
                 folium_static(plot_central_delivery_locations(df_clear), width=1024, height=600)
             with col2:
-                st.write("Localização dos restaurantes")
+                st.write("Restaurant locations:")
                 folium_static(plot_restaurant_locations(df_clear), width=1024, height=600)
             with col3:
-                st.write("HeatMap de pedidos")
+                st.write("Orders HeatMap:")
                 folium_static(plot_orders_heatmap(df_clear), width=1024, height=600)
+    return None
                 
 def viz2():
-    st.write("Em construção")
+    st.title("Delivery View")
+    with st.container(border=True):
+        col1, col2, col3, col4, col5 = st.columns([1, 1, 1, 1, 1])
+        del_metrics = get_metrics_deliveries(df_clear)
+        with col1:
+            st.metric("Lowest rating:", round(del_metrics["ratings_min"], 2))
+            st.metric("Mean delivery distance:", f"{del_metrics["delivery_distance_mean"]:.1f} km")
+        with col2:
+            st.metric("Highest rating:", round(del_metrics["ratings_max"], 2))
+            st.metric("Mean delivery time:", del_metrics["delivery_time_mean"])
+        with col3:
+            st.metric("Mean rating:", round(del_metrics["ratings_mean"], 2))
+            st.metric("Max delivery time:", del_metrics["delivery_time_max"])
+        with col4:
+            st.metric("Median rating:", round(del_metrics["ratings_median"], 2))
+            st.metric("Min delivery time:", del_metrics["delivery_time_min"])
+        with col5:
+            st.metric("Ratings variability:", round(del_metrics["ratings_std_dev"], 2))
+            st.metric("Delivery time variability:", del_metrics["delivery_time_std_dev"])
+    with st.container():
+        col1, col2 = st.columns([1, 1])
+        with col1:
+            st.plotly_chart(plot_deliveries_by_age(df_clear), use_container_width=True)
+        with col2:
+            st.plotly_chart(plot_deliveries_by_vehicle_condition(df_clear), use_container_width=True)
+    with st.container():
+        col1, col2 = st.columns([1, 2])
+        with col1:
+            st.caption("Mean ratings by delivery service:")
+            st.dataframe(get_mean_ratings_by_service(df_clear), use_container_width=True, height=560)
+        with col2:
+            st.caption("Mean ratings and variation by traffic:")
+            st.dataframe(get_means_ratings_by_traffic(df_clear), use_container_width=True)
+            st.caption("Mean ratings and variation by weather:")
+            st.dataframe(get_mean_ratings_by_weather(df_clear), use_container_width=True)
+    return None
+
 def viz3():
-    st.write("Em construção")
+    st.title("Restaurant View")
+    with st.container(border=True):
+        res_metrics = get_metrics_restaurants(df_clear)
+        col1, col2, col3, col4 = st.columns([1, 1, 1, 1])
+        with col1:
+            st.metric("Mean order Pick-up time:", res_metrics["Pick_time_mean"])
+        with col2:
+            st.metric("Slowest order Pick-up time:", res_metrics["Pick_time_max"])
+        with col3:
+            st.metric("Fastest order Pick-up time:", res_metrics["Pick_time_min"])
+        with col4:
+            st.metric("Variation of order Pick-up time:", res_metrics["Pick_time_std_dev"])
+    with st.container():
+        col1, col2, col3 = st.columns([1, 1, 1])
+        with col1:
+            st.caption("Mean Pick-up time by order:")
+            st.dataframe(get_mean_pick_time_by_order(df_clear))
+        with col2:
+            st.caption("Mean Pick-up time by city type:")
+            st.dataframe(get_mean_pick_time_by_city(df_clear))
+        with col3:
+            st.caption("Mean Pick-up time by traffic density:")
+            st.dataframe(get_mean_pick_time_by_traffic(df_clear), height=177)
+        col1, col2 = st.columns([1, 1])
+        with col1:
+            st.caption("Top 10 fastest deliveries:")
+            st.dataframe(get_top_10_fastest_deliveries(df_clear))
+        with col2:
+            st.caption("Top 10 fastest deliveries:")
+            st.dataframe(get_top_10_fastest_deliveries(df_clear, reverse=True))
+    return None
+
 if 'visualizacao' not in st.session_state:
     st.session_state.visualizacao = 1
 if st.session_state.visualizacao == 1:
