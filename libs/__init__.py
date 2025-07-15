faaca9202c552ce65f4b9d36902defa057bc748a
luc457x <lucas7eixeir4@outlook.com>
2025-07-15 20:32:44 -0300

    minor fix

--- libs/__init__.py
+++ libs/__init__.py
@@ -1,5 +1,5 @@
 import builtins
-from .utils import pd, np, haversine, clear_data, load_dataset, check_outliers
+from .utils import pd, np, haversine, clear_data, load_dataset, check_outliers, stringfy_time
 from .metrics import (
     get_metrics_company, get_metrics_deliveries, get_metrics_restaurants, 
     get_mean_ratings_by_service, get_means_ratings_by_traffic, get_mean_ratings_by_weather, 
@@ -7,7 +7,7 @@ from .metrics import (
     get_mean_pick_time_by_traffic, plot_histogram, plot_orders_per_week, plot_orders_per_day, 
     plot_orders_by_traffic, plot_orders_by_traffic_and_city_type, plot_weekly_orders_per_service, 
     plot_central_delivery_locations, plot_restaurant_locations, plot_orders_heatmap, 
-    plot_deliveries_by_age, plot_deliveries_by_vehicle_condition
+    plot_deliveries_by_age, plot_deliveries_by_vehicle_condition, plot_correlation
     )
 
 __all__ = ["pd"]
